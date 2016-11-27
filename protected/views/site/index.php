dee211932c3af622193adb19848f9dc17771a40e
des <des@DESKTOP-Q9JT0H1>
2016-11-27 17:44:17 +0500

    минификация \ чпу

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -18,8 +18,3 @@ $this->pageTitle = Yii::app()->name;
     the <a href="http://www.yiiframework.com/doc/">documentation</a>.
     Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
     should you have any questions.</p>
-<?php 
-$xls = new PHPExcel();
-echo '<pre>';
-var_dump($xls);
-?>
